b09abd02c4f95635e485f4fec9ea9fd05ccd95fc
Hannaroz <hanna.rozmus@outlook.com>
2021-11-18 18:44:06 +0100

    Initial message

---
+++ src/nl/novi/HotelApp/App.java
@@ -0,0 +1,10 @@
+package nl.novi.HotelApp;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Welkom World");
+        System.out.println("My name is Hanna");
+	// write your code here
+    }
+}
