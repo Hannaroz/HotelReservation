6ceae4bb9b8d16d4b66c1180ea9e4cbba73abd4c
Hannaroz <hanna.rozmus@outlook.com>
2021-11-18 22:02:01 +0100

    Mistakes were made

--- src/nl/novi/HotelApp/App.java
+++ src/nl/novi/HotelApp/App.java
@@ -4,7 +4,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Welkom World");
-        System.out.println("My name is Hanna");
+        System.out.println("Blad");
 	// write your code here
     }
 }
